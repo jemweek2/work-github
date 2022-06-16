3c34e767b4021c71ecddead7e5cc4f9c07e261ce
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-06-16 05:07:46 +0000

    model

--- db/migrate/20220615061601_devise_create_customers.rb
+++ db/migrate/20220615061601_devise_create_customers.rb
@@ -13,7 +13,6 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
@@ -31,14 +30,37 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+      # created_at	作成日			datetime
+      # updated_at	更新日			datetime
 
 
       t.timestamps null: false
+      t.string :family_name, null: false
+    # family_name	苗字			string
+      t.string :first_name, null: false
+    # first_name	氏名			string
+      t.string :family_name_kana, null: false
+    # family_name_kana	苗字（カナ入力）			string
+      t.string :first_name_kana, null: false
+    # first_name_kana	氏名(カナ入力)			string
+      t.string :postal_code, null: false
+    # postal_code	郵便番号			string
+      t.string :address, null: false
+    # address	住所			string
+      t.string :phone_number, null: false
+    # phone_number	電話番号			string
+      t.boolean :is_deleted,  null: false, default: ""
+    # is_deleted	退会ステータス			boolean
+
+
     end
 
     add_index :customers, :email,                unique: true
     add_index :customers, :reset_password_token, unique: true
     # add_index :customers, :confirmation_token,   unique: true
     # add_index :customers, :unlock_token,         unique: true
+
+
+
   end
 end
