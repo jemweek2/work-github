72d410465f5601d554567ef1c0f4f081f68f94c5
EC2 Default User <uemana0518@gmail.com>
2022-06-16 07:54:56 +0000

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -13,6 +12,14 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+scope module: :public do
+root to: 'homes#top'
+get 'homes/about'
+end 
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
