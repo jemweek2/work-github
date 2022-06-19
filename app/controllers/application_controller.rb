0cc98a7faba02df89d662787bcbac25bae03e7e6
EC2 Default User <uemana0518@gmail.com>
2022-06-19 01:12:49 +0000

    header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(resource)
+  if resource == :admin
+        admin_root_path
+  else
+       root_path
+  end
+  end
+
+
 
   protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
-  end
 
 end
