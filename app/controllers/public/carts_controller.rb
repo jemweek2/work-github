ec7d597a00d37f2b6ff6872fc165274bdee754fc
EC2 Default User <104713562+sibataichi@users.noreply.github.com>
2022-06-19 07:54:45 +0000

    カート、商品

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -10,4 +10,10 @@ class Public::CartsController < ApplicationController
 
   def create
   end
+
+
+ private
+  def cart_item_params
+      params.require(:cart_item).permit(:product_id, :amount)
+  end
 end
