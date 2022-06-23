d5ce83e618a886caa5513c409b9b178d576e1860
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-06-23 08:01:42 +0000

    second

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,10 @@
 class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.page(params[:page]).per(10)
+    @total = 0
   end
 
   def show
-    #@order = Order.find(params[:id])
+    @order = Order.find(params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum order_status: { waiting:0, confirm: 1, producing: 2, preparing: 3, shipped: 4 }
+  enum order_status: { waiting: 0, confirm: 1, producing: 2, preparing: 3, shipped: 4 }
 
   def subtotal
     product.add_no_tax_price * quantity
